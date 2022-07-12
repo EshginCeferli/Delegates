7b394b1545537e99308a064e8fadc5dc5fd9c0b7
EshginCeferli <eshginij@code.edu.az>
2022-07-13 00:56:32 +0400

    Delegates

---
+++ DelegateLessonReview/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate bool CheckNums(int num);
+        public delegate void ChangeString(string str);
+        public delegate int StrLength(string str);
+        
+        static void Main(string[] args)
+            
+        {
+            #region Predicate,Action
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            //Console.WriteLine(Sum(CheckOdd,arr));
+
+            //Console.WriteLine(Sum(CheckEven, arr));
+
+            //Console.WriteLine(Sum(CheckHigherFive,arr));
+            //List<int> numbers = new List<int>() { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //var result = numbers.FindAll(m => m > 6);
+
+            //var result2 = numbers.FindAll(CheckHigherFive);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Test(StringToUpper, "Cavid");
+            //Test(StringToLower, "Cavid");
+
+            //ChangeString changeString = new ChangeString(StringToLower);
+            //changeString.Invoke("Orxan");
+
+            //ChangeString change = StringToLower;
+            //change += StringToUpper;
+            //change += delegate (string s)
+            // {
+            //     Console.WriteLine(s.ToLower());
+            // };
+            //change += s => Console.WriteLine(s.ToUpper());
+
+            //change("Orxan");
+
+            //Action<int, string> action = StringToUpper;
+            //action += StringToLower;
+            //action(55, "Orxan");
+            #endregion
+            Console.WriteLine(Test(StringLength,"eshgin"));
+
+            
+
+
+
+        }
+        public static int StringLength(string str)
+        {
+            return str.Length;
+        }
+        public static int Test(StrLength func, string str)
+        {
+            return func(str);
+        }
+
+        public static void StringToUpper(int n,string str)
+        {
+            Console.WriteLine(str.ToUpper());
+        }
+        public static void StringToLower(int n,string str)
+        {
+            Console.WriteLine(str.ToLower());
+        }
+
+        public static void Test(ChangeString func, string str)
+        {
+            func(str);
+        }
+
+        public static void Test() => Console.WriteLine("Tested");
+        public static bool CheckOdd(int number)
+        {
+            return (number % 2 != 0);
+        }
+
+        public static bool CheckEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+
+        public static bool CheckHigherFive(int number)
+        {
+            return (number > 5);
+        }
+
+
+        public static int Sum( Predicate<int> predicate, int[] nums)
+        {
+            var sum = 0;
+            foreach (var item in nums)
+            {
+                if (predicate(item))
+                {
+                    sum += item;
+                }
+            }
+            return sum;           
+        }
+
+        //public static int SumEven(CheckNums func, int[] nums)
+        //{
+        //    var sum = 0;
+        //    foreach (var item in nums)
+        //    {
+        //        if (func(item))
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+        //}
+
+        //public static int SumMoreThanFive(CheckNums func, int[] nums)
+        //{
+        //    var sum = 0;
+        //    foreach (var item in nums)
+        //    {
+        //        if (func(item)) 
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+        //}
+    }
+}
---
+++ DelegateTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegateTask
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "Eshgin", "Sadiq", "Orxan", "Seid" };
+
+            Name(CheckEshgin, list);
+        }
+        public static bool CheckEshgin(string name)
+        {
+            return name == "Eshgin";
+        }
+        public static void Name(Predicate<string> predicate, List<string> names)
+        {
+            foreach (var item in names)
+            {
+                if (predicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+    
+}
